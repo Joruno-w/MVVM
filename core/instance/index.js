3b535cce8c2ae75f8a9933b6d5bacfb4552fd4cc
wangshengliang <1710642275@qq.com>
2020-09-11 21:33:07 +0800

    complete renderVnode

--- core/instance/index.js
+++ core/instance/index.js
@@ -1,9 +1,12 @@
 import {initMixin} from "./init.js";
+import {renderMixin} from "./render.js";
 
 function Due(options) {
     this._init(options);
+    this._render();
 }
 
 initMixin(Due);
+renderMixin(Due);
 
 export default Due;
--- core/instance/render.js
+++ core/instance/render.js
@@ -1,6 +1,32 @@
+import {getValue} from "../utils/objectUtil.js";
+
 let template2Vnode = new Map();
 let vnode2Template = new Map();
+export function renderMixin(Due) {
+    Due.prototype._render = function () {
+        renderNode(this,this._vnode);
+    }
+}
 
+export function renderNode(vm,vnode) {
+    if (vnode.nodeType === 3){
+        let templates = vnode2Template.get(vnode);
+        if (templates){
+            let result = vnode.text;
+            for (let i = 0;i < templates.length;i ++){
+                let templateValue = getTemplateValue([vm._data,vnode.env], templates[i]);
+                if (templateValue){
+                    result = result.replace("{{" + templates[i] + "}}", templateValue);
+                }
+            }
+            vnode.elm.nodeValue = result;
+        }
+    }else{
+        for (let i = 0;i < vnode.children.length;i ++){
+            renderNode(vm,vnode.children[i]);
+        }
+    }
+}
 export function prepareRender(vm,vnode) {
     if (vnode == null){
         return;
@@ -16,8 +42,8 @@ export function prepareRender(vm,vnode) {
 }
 
 function analysisTemplateString(vnode) {
-    const templateStrList = vnode.text.match(/{{[ \s\S]+}}/g);
-    for (let i = 0;i < templateStrList && templateStrList.length;i++) {
+    const templateStrList = vnode.text.match(/{{[a-zA-Z0-9._]+}}/g);
+    for (let i = 0;templateStrList && i < templateStrList.length;i++) {
         setTemplate2Vnode(templateStrList[i],vnode);
         setVnode2Template(templateStrList[i],vnode);
     }
@@ -44,7 +70,7 @@ function setVnode2Template(template,vnode) {
 
 function getTemplateName(template) {
     //判断是否有花括号，如果有就去掉，如果没有就直接返回
-    if (template.substring(0,2) === "{{" && template.substring(template.length - 2) === "}}"){
+    if (template.substring(0,2) === "{{" && template.substring(template.length - 2,template.length) === "}}"){
         return template.substring(2,template.length - 2);
     }else{
         return template;
@@ -57,4 +83,14 @@ export function getTemplate2VnodeMap() {
 
 export function getVnode2TemplateMap() {
     return vnode2Template;
+}
+
+function getTemplateValue(objs,templateName) {
+    for (let i = 0;i < objs.length;i ++){
+        let temp = getValue(objs[i],templateName);
+        if (temp != null){
+            return temp;
+        }
+    }
+    return null;
 }
